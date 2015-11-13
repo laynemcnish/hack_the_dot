0c29d6381ac5d06e76e9498536a60847e3925194
Jon Frisby <jfrisby@mrjoy.com>
2015-11-12 19:04:23 -0800

    Move to scaffolding (missed a couple things).
    Sorry @laynemcnish -- I'm leveraging Rails even though it's ugly and kinda the Wrong Way.

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'translator#index'
-
-   resources :translator do
-     collection do
-       get 'hippo'
-       get 'german'
-      end
-   end
+   root 'requests#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
